acb8ef154b6e633739fd62adfaef164bf73473a2
KhimichOlha <vt221_khoo@student.ztu.edu.ua>
2024-04-29 20:04:06 +0300

    Add DemoSubscription

---
+++ lab-2/CreationalDesignPatterns/DemoSubscription/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoSubscription
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
