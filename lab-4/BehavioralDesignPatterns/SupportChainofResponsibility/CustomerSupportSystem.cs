6f3c5a2f92fe562fdcea85d8403b6a31d1709c0b
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2024-05-08 20:25:24 +0300

    Add CustomerSupportSystem

--- lab-4/BehavioralDesignPatterns/SupportChainofResponsibility/CustomerSupportSystem.cs
+++ lab-4/BehavioralDesignPatterns/SupportChainofResponsibility/CustomerSupportSystem.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace SupportChainofResponsibility
 {
-    internal class CustomerSupportSystem
+    public class CustomerSupportSystem
     {
+        private IHandler _firsthandler;
+        public CustomerSupportSystem() 
+        {
+            _firsthandler = new LevelOneHandler();
+            IHandler twohandler = new LevelTwoHandler();
+            IHandler threehandler = new LevelThreeHandler();
+            IHandler fourhandler = new LevelFourHandler();
+            IHandler fivehandler = new LevelFiveHandler();
+            _firsthandler.SetNext(twohandler).SetNext(threehandler).SetNext(fourhandler).SetNext(fivehandler);
+
+        }
+        public void Start()
+        {
+            Console.WriteLine("Дщброго вечора це ваша система підтримки");
+            while (true)
+            {
+                Console.WriteLine("Ведіть від 1 до 4. 1 інформація про ваш номер. 2 інформація про ваш баланс або тариф." +
+                    " 3 Перевірка або поповнення балансу.4 Перевірка балансу. 5 Поповнення балансу. 0 для виходу ");
+                int level;
+                if (!int.TryParse(Console.ReadLine(), out level)||  level < 0||  level > 4)
+                {
+                    Console.WriteLine("Неправельний ввід. Повторіть спробу.");
+                    continue;
+                }
+                if (level == 0)
+                {
+                    break;
+                }
+                _firsthandler.Hendl(level);
+            }
+        }
+
     }
 }
