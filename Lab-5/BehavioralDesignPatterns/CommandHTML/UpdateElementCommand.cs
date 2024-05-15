440fe8f7af97a51c7a872094f1d6747d61a8dcb0
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2024-05-15 20:36:58 +0300

    Add CommandHTML (UpdateElementCommand, RemoveElementCommand, Command, AddElementCommand)

---
+++ Lab-5/BehavioralDesignPatterns/CommandHTML/UpdateElementCommand.cs
@@ -0,0 +1,34 @@
+﻿using LightHtml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandHTML
+{
+    public class UpdateElementCommand : Command
+    {
+        private LightNode _element;
+        private string _newText;
+
+        public UpdateElementCommand(LightNode element, string newText)
+        {
+            _element = element;
+            _newText = newText;
+        }
+
+        public override void Execute()
+        {
+            if (_element is LightTextNode)
+            {
+                var textNode = (LightTextNode)_element;
+                textNode.Text = _newText;
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot update non-text node.");
+            }
+        }
+    }
+}
